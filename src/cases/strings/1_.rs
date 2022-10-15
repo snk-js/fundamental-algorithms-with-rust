5fb5b877ca7c2626cfa26171cdc6518eb6786cbd
juliano <julianoh1909@gmail.com>
2022-10-15 09:31:46 -0300

    add cargo repo and started first case sample

---
+++ src/cases/strings/1_.rs
@@ -0,0 +1,29 @@
+// Complete the solution so that it splits the string into pairs of two characters.
+// If the string contains an odd number of characters then it should replace the missing
+// second character of the final pair with an underscore ('_').
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+// credits: https://www.codewars.com/users/Unnamed
+use itertools::Itertools;
+
+fn solution(s: &str) -> Vec<String> {
+    s.chars()
+        .chunks(2)
+        .into_iter()
+        .map(|c| c.pad_using(2, |_| '_').collect())
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(solution("abcdef"), ["ab", "cd", "ef"]);
+        assert_eq!(solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
+        assert_eq!(solution(""), [] as [&str; 0]);
+    }
+}
